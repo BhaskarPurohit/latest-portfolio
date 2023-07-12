e956ddd0bb173875c76b4fff8ec7eaa2b6beffbd
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-12 23:21:04 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './components/Header'
+import "./styles/app.scss"
 
 const App = () => {
   return (
-    <div>
-      <h1>Bhaskar</h1>
-    </div>
+    <>
+      <Header/>
+    </>
   )
 }
 
