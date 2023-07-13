ef36a0ba111cd08ec7deeb718161df6a9ea8f1c5
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-13 16:47:59 +0530

    new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import "../styles/home.scss"
+import {motion} from "framer-motion"
+
+
 
 const Home = () => {
+
+    const animations = {
+        h1:{
+            initial:{
+                x: "-100%",
+                opactiy: 0,
+            },
+            whileInView:{
+                x:0,
+                opacity: 1,
+            }
+
+        }
+    }
   return (
-    <div id='home'>Home</div>
+    <div id='home'>
+        <section>
+            <div>
+                <motion.h1 {...animations.h1}
+                   >
+                    Hi, I Am <br/> Bhaskar Purohit
+                </motion.h1>
+            </div>
+        </section>
+        <section></section>
+    </div>
   )
 }
 
