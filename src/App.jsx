78414f18b22de8c6b2636cc9c93b010923f77cf1
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-07-14 14:51:48 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './components/Header'
 import Home from "./components/Home"
 import "./styles/app.scss"
+import Work from './components/Work'
 
 const App = () => {
   return (
     <>
       <Header/>
       <Home/>
+      <Work/>
     </>
   )
 }
